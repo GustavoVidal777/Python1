6e22bb2dbd72adee8901c855dfe0c01586ed9261
GustavoVidal777 <gustavo.vidal777@yahoo.com.br>
2026-04-16 14:55:57 -0300

    Continuação da aula 5

---
+++ Projeto5.py
@@ -0,0 +1,45 @@
+lista = [12, 10, 5, 7]
+lista_animal = ['cão', 'gato', 'bode', 'cobra', 'arara']
+# print (lista [2])
+# print (lista_animal [1])
+
+# for x in lista_animal:
+#     print (x)
+
+# soma = 0
+# for x in lista:
+#     print (x)
+#     soma += x
+#     print (soma)
+
+# nova_lista = lista * 3
+# print (nova_lista)
+
+# print (sum(lista)) # somar a lista
+# print (max(lista)) # maior número
+# print (min(lista)) # menor número
+# if 'lobo' in lista_animal: # Validar se valor existe na lista
+#     print ('Existe um lobo na lista')
+# else:
+#     print ('não existe um lobo na lista, será incluido.')
+#     lista_animal.append('lobo')# Incluir animal na lista
+#     print (lista_animal)
+
+# lista_animal.pop(0)# Retira o ultimo dado da lista
+# print (lista_animal)
+# # ou para remover especifico
+# lista_animal.remove('lobo')
+# print (lista_animal)
+
+# lista.sort() # ordena em ordem crescente alfabetica / numica
+# lista_animal.sort()
+# print (lista)
+# print (lista_animal)
+# lista_animal.reverse() # ordena em ordem decrescente alfabetica / numica
+# print (lista_animal)
+
+lista_animal [0] = 'macaco'
+print (lista_animal)
+
+tupla = (1, 10, 12, 15) #Tupla não da pra alterar o resultado
+print (len(tupla)) #quantidade de itens na lista
